42663992ec026c7f4a111fe4cfba9179b257460b
Ugo Pattacini <ugo.pattacini@iit.it>
2018-02-09 17:13:32 +0100

    Update test.sh

--- smoke-test/test.sh
+++ smoke-test/test.sh
@@ -8,7 +8,7 @@ get_helpers=false
 if [ $# -gt 0 ]; then
     if [ "$1" == "--get-helpers" ]; then
         get_helpers=true
-    elif [ "$1" == "--help" ]; then 
+    elif [ "$1" == "--help" ]; then
         echo ""
         echo "Usage: $0 [--get-helpers]"
         echo "\"--get-helpers\" specifies to force downloading helper tools anew"
@@ -38,9 +38,9 @@ fi
 build_dir=$(pwd)
 
 if [ $? -eq 0 ]; then
-    if [ -f ${test_dir}/test-type ]; then
-        test_type=$(head -1 ${test_dir}/test-type)
-        ./helpers/scripts/smoke-test-${test_type}.sh $build_dir $code_dir $test_dir
+    if [ -f "${test_dir}"/test-type ]; then
+        test_type=$(head -1 "${test_dir}"/test-type)
+        ./helpers/scripts/smoke-test-${test_type}.sh "$build_dir" "$code_dir" "$test_dir"
         ret=$?
     else
         echo -e "${red}test-type is missing!${nc}"
